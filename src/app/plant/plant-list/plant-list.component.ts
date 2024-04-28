d00d2ef049d0b728edb9ebe9611bf00d24089621
Manuel Sanchez <mg.sanchezb1@uniandes.edu.co>
2024-04-28 18:53:46 -0500

    Punto2-Total-Plantas

--- src/app/plant/plant-list/plant-list.component.ts
+++ src/app/plant/plant-list/plant-list.component.ts
@@ -11,14 +11,20 @@ import { PlantService } from '../services/plant.service';
 })
 export class PlantListComponent implements OnInit {
   plants: Plant[] = [];
+  totalInterior: number = 0;
+  totalExterior: number = 0;
 
   constructor(private plantService: PlantService) { }
 
   ngOnInit(): void {
     this.plantService.getPlants().subscribe((data: Plant[]) => {
       this.plants = data;
-
+      this.calcularTotales();
     });
+  }
+  calcularTotales(): void {
+    this.totalInterior = this.plants.filter(plant => plant.tipo === 'Interior').length;
+    this.totalExterior = this.plants.filter(plant => plant.tipo === 'Exterior').length;
   }
 
 }
